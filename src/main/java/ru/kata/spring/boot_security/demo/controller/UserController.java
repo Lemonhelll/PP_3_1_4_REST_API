6b2c288d8d8df8ee35655224cbc3f4d7dc331a54
Vladimir Zolotov <volodya.zolotov.14@mail.ru>
2022-11-21 18:16:32 +0300

    correct mistakes комментарии и пустые страницы html оставил на всякий, если нужно удалю

--- src/main/java/ru/kata/spring/boot_security/demo/controller/UserController.java
+++ src/main/java/ru/kata/spring/boot_security/demo/controller/UserController.java
@@ -3,11 +3,13 @@ package ru.kata.spring.boot_security.demo.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import ru.kata.spring.boot_security.demo.service.UserServiceImpl;
 
 import java.security.Principal;
 
 @Controller
+@RequestMapping("/user")
 public class UserController {
     private final UserServiceImpl userService;
 
@@ -15,7 +17,7 @@ public class UserController {
         this.userService = userService;
     }
 
-    @GetMapping("/user")
+    @GetMapping()
     public String getUserProfile(Model model, Principal principal) {
         model.addAttribute("user", userService.findByUsername(principal.getName()));
         return "userInfoPage";
